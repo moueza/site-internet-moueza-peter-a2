96d95c983f8cf1a8f9d887a02711d02cf01ca27f
moueza <mouezapeter@gmail.com>
2019-08-04 22:05:21 +0200

    See #1 less (CSS) ; main menu tennis-match-counter components

---
+++ peter/src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { TennisMatchCounterComponent } from './tennis-match-counter/tennis-match-counter.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MenuComponent,
+    TennisMatchCounterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
