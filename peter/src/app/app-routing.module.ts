5f68fd92bdcd6bbd1ba4b486e28d27a16791deed
moueza <mouezapeter@gmail.com>
2019-08-04 22:55:14 +0200

    Resolves #2 blank page : no list in <p>...so div ; OK

--- peter/src/app/app-routing.module.ts
+++ peter/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { MenuComponent } from './menu/menu.component';
+import { TennisMatchCounterComponent } from './tennis-match-counter/tennis-match-counter.component';
+
+const routes: Routes = [
+ { path: 'menu', component: MenuComponent },
+ { path: 'arbitrage', component: TennisMatchCounterComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
